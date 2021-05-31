7bf9cac14b232290c4deb2f85ffe7f5c0b92fc9d
InterLink Intern <intern@intern-pc.interlink.office>
2021-05-31 17:50:05 +0300

    second

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,29 @@ namespace EchoApp
         }*/
     }
 
+    public class Connector
+    {
+        public Objects obj {get; set;}
+        public int x {get; set;}
+        public int y {get; set;}
+        public int turn {get; set;}
+        public Connector(Objects obj_, int x_, int y_, int z_, int turn_)
+        {
+            obj = obj_;
+            y = x_;
+            x = y - y_;
+            turn = turn_;
+        }
+
+        public Connector(Objects obj_, int x_, int y_, int turn_)
+        {
+            obj = obj_;
+            x = x_;
+            y = y_;
+            turn = turn_;
+        }
+    }
+    
     public class Objects
     {
         public int speed {get; set;}
@@ -36,14 +59,48 @@ namespace EchoApp
         public bool doubleCell {get; set;}
         public List<Point> canMove {get; set;}
 
-        /*public Objects(int x, int y, int speed_, string image, bool double_)
+        public Objects(int speed_, string img_, bool doubleCell_)
         {
             speed = speed_;
-            img = image;
-            doubleCell = double_;
+            img = img_;
+            doubleCell = doubleCell_;
             canMove = new List<Point>();
-        }*/
+        }
+    }
 
+    public class HexArray
+    {
+        public Objects[][][] array {get; set;}
+
+        public HexArray()
+        {
+            array = new Objects[30][][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Objects[30][];  
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = new Objects[30];
+                }
+            }
+        }
+
+        public Objects this[int x, int y, int z]
+        {
+            get => array[x + 14][y + 14][z + 14];
+            set => array[x + 14][y + 14][z + 14] = value; 
+        }
+
+        public Objects this[int x, int y]
+        {
+            get => this[y, x - y, -(x + y)];
+            //set => array[y][x - y][-(x + y)] = value;
+            set
+            {
+                this[y, x - y, -(x + y)] = value;
+                Console.WriteLine((y) + "; " + (x - y) + "; " + (-(x + y)));
+            }
+        }
     }
 
     public class Startup
@@ -104,20 +161,32 @@ namespace EchoApp
 #endregion
             app.UseFileServer();
         }
+
 #region Echo
         private async Task Echo(HttpContext context, WebSocket webSocket)
         {
             Point p;
-            Objects[,] objects = new Objects[15, 11];
-            objects[10, 10] = new Objects(/*3, 4, 3, "https://i.ibb.co/rGJc40v/Zealot.png", false*/);
-            objects[10, 10].speed = 3;
-            objects[10, 10].img = "https://i.ibb.co/rGJc40v/Zealot.png";
-            objects[10, 10].doubleCell = false;
-            objects[10, 10].canMove = new List<Point>();
+            
+            HexArray hexArray = new HexArray();
+            hexArray[0, 0, 0] = new Objects(3, "https://i.ibb.co/rGJc40v/Zealot.png", false);
+            
+            /*hexArray[0, 0, 0].speed = 3;
+            hexArray[0, 0, 0].img = "https://i.ibb.co/rGJc40v/Zealot.png";
+            hexArray[0, 0, 0].doubleCell = false;
+            hexArray[0, 0, 0].canMove = new List<Point>();*/
+            //Objects obj = new Objects();
+            
+            /*obj.speed = 3;
+            obj.img = "https://i.ibb.co/rGJc40v/Zealot.png";
+            obj.doubleCell = false;
+            obj.canMove = new List<Point>();*/
             /*objects[4, 9] = new Objects(4, 9, 5, "https://i.ibb.co/263MvmM/Angel.png", false);
             objects[11, 7] = new Objects(11, 7, 3, "https://i.ibb.co/s5CCHZj/Royal-Griffin.png", true);*/
             var buffer = new byte[1024 * 4];
             WebSocketReceiveResult result;
+
+            int turn = 0;
+            
             while (true)
             {
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
@@ -134,10 +203,19 @@ namespace EchoApp
                 p = (Point)JsonSerializer.Deserialize(new ReadOnlySpan<byte>(buffer, 0, result.Count), typeof(Point));
                 //p.x++;
                 
-                await webSocket.SendAsync(new ArraySegment<byte>(JsonSerializer.SerializeToUtf8Bytes(p, typeof(Point))),
+                hexArray[p.x, p.y] = new Objects(2, "https://i.ibb.co/j8qr53X/pess.png", false);
+                Console.WriteLine("fdghajuibdgfeabgidbasgdsbtgrou");
+                /*await webSocket.SendAsync(new ArraySegment<byte>(JsonSerializer.SerializeToUtf8Bytes(
+                    new Connector[1]{new Connector(hexArray[0, 0, 0], 0, 0, 0)}, typeof(Connector[]))),
                     result.MessageType, result.EndOfMessage, CancellationToken.None);
+                    Console.WriteLine("sent");*/
+                    await webSocket.SendAsync(new ArraySegment<byte>(JsonSerializer.SerializeToUtf8Bytes(
+                    new Connector(hexArray[p.x, p.y], p.x, p.y, turn), typeof(Connector))),
+                    result.MessageType, result.EndOfMessage, CancellationToken.None);
+                    turn = turn == 0 ? 1 : 0;
                     Console.WriteLine("sent");
             }
+            Console.WriteLine("closed");
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
 #endregion
